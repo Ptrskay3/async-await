d0387ba3a1a4eea266ca62cae1a8d6d19ee53f57
peter leeh <leeh123peter@gmail.com>
2023-10-31 13:49:05 +0100

    add scoped threads

---
+++ examples/all/manual_scoped.rs
@@ -0,0 +1,19 @@
+//! ```cargo
+//! [dependencies]
+//! reqwest = { version = "0.11", features = ["blocking"] }
+//! ```
+
+fn main() {
+    std::thread::scope(|scope| {
+        for name in &["A", "B", "C", "D"] {
+            scope.spawn(move || println!("{}", get_user(name)));
+        }
+    });
+}
+
+fn get_user(name: &str) -> String {
+    reqwest::blocking::get(format!("http://127.0.0.1:3001/{name}"))
+        .unwrap()
+        .text()
+        .unwrap()
+}
